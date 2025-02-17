33999bf507aed9266e4e98511ac47e91dd0bc0da
KanuriLakshmiPrasanna <143861642+KanuriLakshmiPrasanna@users.noreply.github.com>
2025-02-17 20:29:43 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -3,3 +3,10 @@ for i in range(5):
     for j in range(i+1):
         print("*",end="")
     print()
+
+#To print the following Pattern
+'''****
+   ****
+   ****'''
+for i in range(4):
+    print('*'*4)
