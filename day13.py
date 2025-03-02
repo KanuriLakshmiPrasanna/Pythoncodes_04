8321b9c68d80a7c51c7fcf9a4157b5910ee35ab2
KanuriLakshmiPrasanna <143861642+KanuriLakshmiPrasanna@users.noreply.github.com>
2025-03-02 22:45:03 +0530

    Add files via upload

---
+++ day13.py
@@ -0,0 +1,9 @@
+#To return the ASCII value 
+def countercodepart(char):
+    return ord(char)
+    
+    
+    
+the_char=input("Enter to check the counrercode:")
+result=countercodepart(the_char)
+print(result)
