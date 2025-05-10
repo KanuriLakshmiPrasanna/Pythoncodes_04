a2dd14dad02692c5101007337a24361987b98c99
KanuriLakshmiPrasanna <143861642+KanuriLakshmiPrasanna@users.noreply.github.com>
2025-05-10 23:32:59 +0530

    Add files via upload

---
+++ day 83.py
@@ -0,0 +1,8 @@
+'''Chef has bag with items in different order now he has forget the first item he has put into the bag and also he wants to put an item numbered 6 into the bag.Help him to do these tasks'''
+
+Chef_list = [9, 5, 1, 3, 8]
+print("List:", Chef_list)
+print("Length:", len(Chef_list))
+print("Element at index 0:",Chef_list[0])
+Chef_list.append(6)
+print("List after append:",Chef_list)
